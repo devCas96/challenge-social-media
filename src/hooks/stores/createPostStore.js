ff6c653ca8d52831aea9b55a4120fac2662d1db6
Sebastián Castaño <sebas.cas96@hotmail.com>
2024-01-11 11:15:42 -0500

    fix: Filter by tags keeped when load more clicked.

--- src/hooks/stores/createPostStore.js
+++ src/hooks/stores/createPostStore.js
@@ -1,4 +1,6 @@
 export const createPostStore = (set) => ({
   postList: null,
+  tagId: null,
   setPostList: (_posts) => set(() => ({ postList: _posts })),
+  setTagId: (_tagId) => set(() => ({ tagId: _tagId })),
 });
